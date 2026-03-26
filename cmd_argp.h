d94857489dc8498bc58237db6be119d90b5eae05
John Bush <jbush.freelance@gmail.com>
2026-03-26 15:48:22 -0700

    wtf did I do?

--- cmd_argp.h
+++ cmd_argp.h
@@ -5,15 +5,20 @@
 const char * argp_program_version = "version 0.5";
 const char * argp_program_bug_address = "jxb@bush-tech.net";
 
-struct ula_options {
+struct ula_opts{
    char iface_name[MAX_IF_NAME_LEN];
+};
+
+struct argp_options ula_options[] = {
+   {"iface", 'i', "INTERFACE", 0, "Network interface with MAC to use"}, 
+   {0}
 };
 
 struct argp ula_argp {
 
 };
 
-static int parse_opt(int key, char * arg, struct argp_state * state);
+static int ula_parser(int key, char * arg, struct argp_state * state);
 
 int ula_arg_parser(int argc, char ** argv, struct ula_options * opts);
 
